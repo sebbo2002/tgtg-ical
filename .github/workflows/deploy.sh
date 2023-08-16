fe1948d624dfe9bddf1f8a2e3fddfb5741aa2832
Sebastian Pekarek <mail@sebbo.net>
2023-08-16 17:02:53 +0200

    feat: Add `npm run deploy` script to deploy directly

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+rsync -avzr --delete \
+    --exclude node_modules --exclude .git --exclude .idea --exclude .nyc_output --exclude docs \
+    ./ ${DEPLOYMENT_URL:-tgtg-ical.sebbo.net}:~/apps/tgtg-ical/package/
+
+ssh ${DEPLOYMENT_URL:-tgtg-ical.sebbo.net} \
+    "cd ~/apps/tgtg-ical/package/ && npm ci && supervisorctl restart tgtg-ical && tail -n 50 ~/logs/supervisord.log"

cada207eff16e30b7975fe8f0e42300a86a6ae7c
Sebastian Pekarek <mail@sebbo.net>
2023-08-16 17:28:14 +0200

    ci: Fix deployment :/

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,5 +5,5 @@ rsync -avzr --delete \
     --exclude node_modules --exclude .git --exclude .idea --exclude .nyc_output --exclude docs \
     ./ ${DEPLOYMENT_URL:-tgtg-ical.sebbo.net}:~/apps/tgtg-ical/package/
 
-ssh ${DEPLOYMENT_URL:-tgtg-ical.sebbo.net} \
+ssh -v ${DEPLOYMENT_URL:-tgtg-ical.sebbo.net} \
     "cd ~/apps/tgtg-ical/package/ && npm ci && supervisorctl restart tgtg-ical && tail -n 50 ~/logs/supervisord.log"

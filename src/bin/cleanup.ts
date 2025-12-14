c74e13a413b243629ad254d03688d4e39fc9628f
Sebastian Pekarek <mail@sebbo.net>
2025-12-14 21:00:31 +0100

    feat: Disconnect database after cleanup

--- src/bin/cleanup.ts
+++ src/bin/cleanup.ts
@@ -2,9 +2,12 @@
 'use strict';
 
 /* istanbul ignore file */
+import prisma from '../lib/db.js';
 import Parser from '../lib/parser.js';
 
-Parser.runCleanup().catch((error) => {
+Parser.runCleanup().then(() => {
+    return prisma.$disconnect();
+}).catch((error) => {
     console.error(error);
     process.exit(1);
 });
